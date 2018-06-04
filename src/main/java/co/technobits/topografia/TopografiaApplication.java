97048c116f70617c4955eba0d31deea737a3a0d1
Daniela sanmiguel <dasamo1010@gmail.com>
2018-06-03 19:41:49 -0500

    fist commit

---
+++ src/main/java/co/technobits/topografia/TopografiaApplication.java
@@ -0,0 +1,12 @@
+package co.technobits.topografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopografiaApplication.class, args);
+	}
+}
